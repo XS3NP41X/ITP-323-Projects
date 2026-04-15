bf21494cb6081410dbfc87d5ca8d1be8a693a44f
XS3NP41X <saraniapatrick@gmail.com>
2026-04-15 19:00:51 +0800

    commit

--- Sarania_logic_checker_app.js
+++ Sarania_logic_checker_app.js
@@ -9,3 +9,9 @@ let isTimmyAGamer = false;
 if (isTimmyAGamer) {
   console.log("Timmy loves to play World of Warcraft.");
 }
+
+let timmyAge = 17; // currently 17, old enough to drive
+
+if (timmyAge >= 16) {
+  console.log("Timmy is old enough to drive.");
+}
