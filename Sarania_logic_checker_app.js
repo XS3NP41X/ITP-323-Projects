43fbe287abe764b55913f76b3278aa81cd109d57
XS3NP41X <saraniapatrick@gmail.com>
2026-04-15 19:00:00 +0800

    commit

--- Sarania_logic_checker_app.js
+++ Sarania_logic_checker_app.js
@@ -1,9 +1,1 @@
-let truthyOrFalsy = true;
-
-console.log(Boolean(truthyOrFalsy)); // true
-
-truthyOrFalsy = "freeCodeCamp";
-console.log(Boolean(truthyOrFalsy));
-
-truthyOrFalsy = "";
-console.log(Boolean(truthyOrFalsy));
+let hasDeveloperJob = true;
