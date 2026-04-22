66a3fc3c1da3446145a4da44052967636c0843e0
XS3NP41X <saraniapatrick@gmail.com>
2026-04-22 18:00:51 +0800

    commit

--- Sarania_Calculator_Laboratory.js
+++ Sarania_Calculator_Laboratory.js
@@ -1,4 +1,4 @@
-// Step 10
+// Step 11
 function calculateSum(num1, num2) {
   return num1 + num2;
 }
@@ -10,3 +10,7 @@ console.log(calculateSum(5, 5));
 function calculateDifference(num1, num2) {
   return num1 - num2;
 }
+
+console.log(calculateDifference(22, 5));
+console.log(calculateDifference(12, 1));
+console.log(calculateDifference(17, 9));
