e64bcf0ee7e1774cbab0915852cbe9e47a0fd5d3
XS3NP41X <saraniapatrick@gmail.com>
2026-04-22 17:59:23 +0800

    commit

--- Sarania_Calculator_Laboratory.js
+++ Sarania_Calculator_Laboratory.js
@@ -1,6 +1,8 @@
-// Step 8
+// Step 9
 function calculateSum(num1, num2) {
   return num1 + num2;
 }
 
 console.log(calculateSum(2, 5));
+console.log(calculateSum(10, 10));
+console.log(calculateSum(5, 5));
